bb3b8e95199b1f22131bb64433046f3162019ab6
xuqiyong <jeffrey6052@163.com>
2012-03-23 14:42:08 +0800

    jeffrey

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,8 +28,8 @@ module ApplicationHelper
       cookies[:user_role] = {:value => "", :path => "/", :secure  => false}
       orders = Order.find(:all, :conditions => ["user_id = ? and status = #{Order::STATUS[:NOMAL]}", user_id.to_i])
       orders.each do |order|
-        if order.types == Order::TYPES[:CHARGE] or order.types == Order::TYPES[:OTHER] or
-            order.types == Order::TYPES[:ACCREDIT] or order.types == Order::TYPES[:RENREN] or order.types == Order::TYPES[:SINA]
+        if order.types == Order::TYPES[:CHARGE] or order.types == Order::TYPES[:OTHER] or order.types == Order::TYPES[:ACCREDIT] or
+            order.types == Order::TYPES[:RENREN] or order.types == Order::TYPES[:SINA] or order.types == Order::TYPES[:MUST]
           this_order = "#{order.category_id}=#{Order::USER_ORDER[:VIP]}"
           cookies[:user_role] = cookies[:user_role].empty? ? this_order : (cookies[:user_role] + "&" + this_order)
         elsif order.types == Order::TYPES[:TRIAL_SEVEN]
