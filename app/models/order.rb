766133ba91521dd2c01b597cf32f43e3d62f8cc3
lifang <santhe_2004@126.com>
2012-04-06 16:53:27 +0800

    update detial

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order< ActiveRecord::Base
   belongs_to :user
   STATUS = {:NOMAL => 1, :INVALIDATION => 0} #1 正常  0 失效
   TYPES = {:CHARGE => 1, :OTHER => 0,:TRIAL_SEVEN => 2, :ACCREDIT => 3,:COMPETE => 4,:RENREN => 5,:SINA => 6,:MUST=>7,:BAIDU=>8,:QQ=>9} #1 付费  0 其它 2 七天试用
-  TYPE_NAME = {1 => "付费", 0 => "其它",2=>"七天试用",3=>"授权码",4=>"模考大赛",5=>"人人分享",6=>"新浪分享",7=>"必过挑战",8=>"百度分享",9=>"qq"}
+  TYPE_NAME = {1 => "付费", 0 => "其它",2=>"七天试用",3=>"授权码",4=>"模考大赛",5=>"人人分享",
+    6=>"新浪分享",7=>"必过挑战",8=>"百度分享",9=>"QQ分享"}
   USER_ORDER = {:VIP => 0, :TRIAL => 1, :NOMAL => 2} #根据order类型和状态判断当前用户的情况：0 vip  1 试用用户  2 普通用户
 end
